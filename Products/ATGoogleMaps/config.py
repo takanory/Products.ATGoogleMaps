eb3a4f08741bcabd01907446475365fd150719fd
takanori <takanori@22bc0ad7-f20f-0410-8e9b-a20c9b70970d>
2009-06-10 04:09:15 +0000

    remove test class
    git-svn-id: http://svn.takanory.net/repos/Products.ATGoogleMaps/trunk@215 22bc0ad7-f20f-0410-8e9b-a20c9b70970d

--- Products/ATGoogleMaps/config.py
+++ Products/ATGoogleMaps/config.py
@@ -7,5 +7,4 @@ ADD_PERMISSIONS = {
     # -*- extra stuff goes here -*-
     'GMarker': 'Products.ATGoogleMaps: Add GMarker',
     'GMap': 'Products.ATGoogleMaps: Add GMap',
-    'A Google Map type': 'Products.ATGoogleMaps: Add A Google Map type',
 }
