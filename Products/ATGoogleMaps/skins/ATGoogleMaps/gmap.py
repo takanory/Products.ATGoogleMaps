06e72ca4f3839d0719f9e2a48d48d0f88e916c23
takanori <takanori@22bc0ad7-f20f-0410-8e9b-a20c9b70970d>
2010-01-29 12:50:10 +0000

    google maps api v2 -> v3
    git-svn-id: http://svn.takanory.net/repos/Products.ATGoogleMaps/trunk@256 22bc0ad7-f20f-0410-8e9b-a20c9b70970d

--- Products/ATGoogleMaps/skins/ATGoogleMaps/gmap.py
+++ Products/ATGoogleMaps/skins/ATGoogleMaps/gmap.py
@@ -1,32 +1,64 @@
-print '<script type="text/javascript">'
-print "//<![CDATA["
-print "window.onload = function() {"
-print "    if (GBrowserIsCompatible()) {"
-print "        var map = createMap('map', %s, %s, %d, '%s', %s, %s, %s, '%s');" % (
-    context.center['latitude'], context.center['longitude'],
-    context.zoom, context.mapControl,
-    str(context.typeControl).lower(),
-    str(context.scaleControl).lower(),
-    str(context.overviewControl).lower(),
-    context.mapType,
-    )
+lat = context.center['latitude']
+lng = context.center['longitude']
+zoom = context.zoom
 
+print """<script type="text/javascript">
+  function initialize() {
+    var latlng = new google.maps.LatLng(%s, %s);
+    var myOptions = {
+      zoom: %s,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+""" % (lat, lng, zoom)
+
+# custom marker javascript
+print """
+      url = "%s";
+      var image = new google.maps.MarkerImage(url + '/gmap_marker.png',
+        new google.maps.Size(20, 34),
+        new google.maps.Point(0,0),
+        new google.maps.Point(8,34));
+      var shadow = new google.maps.MarkerImage(url + '/gmap_shadow.png',
+        new google.maps.Size(37, 34),
+        new google.maps.Point(0,0),
+        new google.maps.Point(9, 34));
+      var shape = {
+        coord: [1, 1, 1, 20, 18, 20, 18 , 1],
+        type: 'poly'
+      }
+       
+
+""" % context.absolute_url()
+
+# marker javascript
 for marker in context.objectValues(['GMarker']):
-    try:
-        latitude = marker.point['latitude']
-        longitude = marker.point['longitude']
-        print "        var tabs = ["
-        print "          new GInfoWindowTab('%s', document.getElementById('marker_html_%s').innerHTML)" % (marker.tab1 , marker.id)
-        if marker.detail:
-            print "          ,new GInfoWindowTab('%s', document.getElementById('detail_html_%s').innerHTML)" % (marker.tab2, marker.id)
-        print "        ];"
-        print "        addMarker(map, tabs, '%s', %s, %s);" % (marker.id, latitude, longitude)
-    except:
-        continue
-print "    }"
-print "}"
-print "//]]>"
-print "</script>"
+        lat = marker.point['latitude']
+        lng = marker.point['longitude']
+
+        print """
+    var infowindow_%s = new google.maps.InfoWindow({
+      content: '<h1>%s</h1>%s'
+    });
+
+    var marker_%s = new google.maps.Marker({
+      position: new google.maps.LatLng(%s, %s),
+      map: map,
+      title: "%s",
+      icon: image,
+      shape: shape,
+      shadow: shadow,
+    });
+
+    google.maps.event.addListener(marker_%s, 'click', function() {
+      infowindow_%s.open(map, marker_%s);
+    });
+        """ % (marker.id, marker.Title(), marker.Description(), marker.id, lat, lng, marker.Title(), marker.id, marker.id, marker.id)
+
+print """
+  }
+  window.onload = initialize;
+</script>"""
 
 return printed
-
