6e38d06f568a06541eac54e86ab0ead14480b21d
takanori <takanori@22bc0ad7-f20f-0410-8e9b-a20c9b70970d>
2010-02-15 15:54:09 +0000

    add marker and infowindow code
    git-svn-id: http://svn.takanory.net/repos/Products.ATGoogleMaps/trunk@267 22bc0ad7-f20f-0410-8e9b-a20c9b70970d

--- Products/ATGoogleMaps/skins/ATGoogleMaps/gmap.py
+++ Products/ATGoogleMaps/skins/ATGoogleMaps/gmap.py
@@ -1,3 +1,27 @@
+site_encoding =  context.plone_utils.getSiteEncoding()
+
+def add_markers(js, markers):
+    #js.extend(('function addMarker()'))
+    for marker in markers:
+        lat = marker.point['latitude']
+        lng = marker.point['longitude']
+        js.extend(('  var marker_%s = new google.maps.Marker({' % marker.id,
+                   '    position: new google.maps.LatLng(%s, %s),' % (lat, lng),
+                   '    map: map,',
+                   '    title: "%s"' % unicode(marker.Title(), site_encoding),
+#                   '    icon: image',
+                   '  });',
+                   ))
+        js.extend(('  var infowindow_%s = new google.maps.InfoWindow({' % marker.id,
+                   '    content: "<h2>%s</h2>"' % unicode(marker.Title(), site_encoding),
+                   '  });',
+                   "  google.maps.event.addListener(marker_%s, 'click', function() {" % marker.id,
+                   '    infowindow_%s.open(map, marker_%s)' % (marker.id, marker.id),
+                   '  });',
+                   ))
+#      shape: shape,
+#      shadow: shadow,
+
 # create initialize method
 def initialize_func(js):
     lat = context.center['latitude']
@@ -6,7 +30,7 @@ def initialize_func(js):
 	       '  var myOptions = {',
 	       '    zoom: %s,' % context.zoom,
 	       '    center: new google.maps.LatLng(%s, %s),' % (lat, lng),
-	       '    mapTypeId: google.maps.MapTypeId.%s,' % context.mapType
+	       '    mapTypeId: google.maps.MapTypeId.%s,' % context.mapType,
 	       ))
 
     # set mapTypeControl
@@ -32,57 +56,31 @@ def initialize_func(js):
     js.extend(('  };',
 	       '  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);',
 	       ))
+    add_markers(js, context.objectValues(['GMarker']))
+    js.append('}')
     
 js = ['<script type="text/javascript">']
 initialize_func(js)
-print "\n".join(js)
+js.append('google.maps.event.addDomListener(window, "load", initialize);')
+js.append('</script>')
+
+return "\n".join(js)
 
 # custom marker javascript
-print """
-      url = "%s";
-      var image = new google.maps.MarkerImage(url + '/gmap_marker.png',
-        new google.maps.Size(20, 34),
-        new google.maps.Point(0,0),
-        new google.maps.Point(8,34));
-      var shadow = new google.maps.MarkerImage(url + '/gmap_shadow.png',
-        new google.maps.Size(37, 34),
-        new google.maps.Point(0,0),
-        new google.maps.Point(9, 34));
-      var shape = {
-        coord: [1, 1, 1, 20, 18, 20, 18 , 1],
-        type: 'poly'
-      }
-       
+#print """
+#      url = "%s";
+#      var image = new google.maps.MarkerImage(url + '/gmap_marker.png',
+#        new google.maps.Size(20, 34),
+#        new google.maps.Point(0,0),
+#        new google.maps.Point(8,34));
+#      var shadow = new google.maps.MarkerImage(url + '/gmap_shadow.png',
+#        new google.maps.Size(37, 34),
+#        new google.maps.Point(0,0),
+#        new google.maps.Point(9, 34));
+#      var shape = {
+#        coord: [1, 1, 1, 20, 18, 20, 18 , 1],
+#        type: 'poly'
+#      }
+#""" % context.absolute_url()
 
-""" % context.absolute_url()
 
-# marker javascript
-for marker in context.objectValues(['GMarker']):
-        lat = marker.point['latitude']
-        lng = marker.point['longitude']
-
-        print """
-    var infowindow_%s = new google.maps.InfoWindow({
-      content: '<h1>%s</h1>%s'
-    });
-
-    var marker_%s = new google.maps.Marker({
-      position: new google.maps.LatLng(%s, %s),
-      map: map,
-      title: "%s",
-      icon: image,
-      shape: shape,
-      shadow: shadow,
-    });
-
-    google.maps.event.addListener(marker_%s, 'click', function() {
-      infowindow_%s.open(map, marker_%s);
-    });
-        """ % (marker.id, marker.Title(), marker.Description(), marker.id, lat, lng, marker.Title(), marker.id, marker.id, marker.id)
-
-print """
-  }
-  window.onload = initialize;
-</script>"""
-
-return printed
