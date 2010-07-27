c7b9a5fdc621c285f612fd0bf7f77546ee2559b2
takanori <takanori@22bc0ad7-f20f-0410-8e9b-a20c9b70970d>
2010-07-27 16:47:29 +0000

    simple polyline support
    git-svn-id: http://svn.takanory.net/repos/Products.ATGoogleMaps/trunk@332 22bc0ad7-f20f-0410-8e9b-a20c9b70970d

--- Products/ATGoogleMaps/content/GPolyline.py
+++ Products/ATGoogleMaps/content/GPolyline.py
@@ -30,10 +30,32 @@ schema = Schema((
                 ),
     FloatField('opacity',
                default=1.0,
+               widget=StringWidget(
+                   label='Opacity',
+                   label_msgid='label_opacity',
+                   description_msgid='help_opacity',
+                   i18n_domain='googlemaps',
+                   size=8,
+                   )
                ),
     IntegerField('weight',
-                 default=2,
+                 default=5,
+                 widget=StringWidget(
+                     label='Weight',
+                     label_msgid='label_weight',
+                     description_msgid='help_weight',
+                     i18n_domain='googlemaps',
+                     size=8,
+                     )
                  ),
+    LinesField('coordinates',
+               widget=LinesWidget(
+                   label='Coordinates',
+                   label_msgid='label_path',
+                   description_msgid='help_path',
+                   i18n_domain='googlemaps',
+                   )
+               ),
     ),)
 gpolyline_schema = getattr(ATCTContent, 'schema', Schema(())).copy() + schema.copy()
 finalizeATCTSchema(gpolyline_schema)
@@ -57,5 +79,13 @@ class GPolyline(ATCTContent):
         except:
             pass
         return latlng
+
+    # return coordinates array string
+    def getCoordinatesArray(self):
+        coordinates_array = "["
+        for coordinate in self.coordinates:
+             coordinates_array += "[%s]," % coordinate
+        coordinates_array = coordinates_array[:-1] + "]"
+        return coordinates_array
 
 registerType(GPolyline, PROJECTNAME)
--- Products/ATGoogleMaps/skins/ATGoogleMaps/gmap.py
+++ Products/ATGoogleMaps/skins/ATGoogleMaps/gmap.py
@@ -1,11 +1,19 @@
 site_encoding =  context.plone_utils.getSiteEncoding()
 
+def add_polylines(js, items):
+    for index in range(len(items)):
+        polyline = items[index].getObject()
+        js.append('  var polyline_%d = createPolyline(map, "%s", %f, %d, "%s");'
+                  % (index, polyline.color, polyline.opacity, polyline.weight, polyline.title))
+        js.append('  polyline_%d.setPath(createPath(%s));' % (index, polyline.getCoordinatesArray()))
+    
 def add_markers(js, items):
     for index in range(len(items)):
         marker = items[index].getObject()
         lat = marker.point['latitude']
         lng = marker.point['longitude']
-        js.append('  var marker_%d = createMarker(map, %s, %s, "%d", "%s");' % (index, lat, lng, index, marker.title))
+        js.append('  var marker_%d = createMarker(map, %s, %s, "%d", "%s");'
+                  % (index, lat, lng, index, marker.title))
 
 # create initialize method
 def initialize_func(js):
@@ -15,7 +23,8 @@ def initialize_func(js):
                '  var map = createMap("map_canvas", %s, %s, %s, "%s", "%s", "%s");' % (lat, lng, context.zoom, context.mapType, context.mapTypeControl, context.navigationControl),
                ))
 
-    add_markers(js, context.getFolderContents())
+    add_markers(js, context.getFolderContents({'portal_type': 'GMarker'}))
+    add_polylines(js, context.getFolderContents({'portal_type': 'GPolyline'}))
 
     js.append('}')
     
